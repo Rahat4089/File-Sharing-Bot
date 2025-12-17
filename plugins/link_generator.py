f17a4d07e7cad0bb299eff515f90341e455e9478
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2025-12-17 23:58:30 +0600

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -66,9 +66,3 @@ async def link_generator(client: Client, message: Message):
 
 
 
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
