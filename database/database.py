55e4d986fcebc5e679a05ab9c30dda09640321b3
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2025-12-18 00:04:52 +0600

    Update database.py

--- database/database.py
+++ database/database.py
@@ -30,13 +30,3 @@ async def del_user(user_id: int):
 
 
 
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
