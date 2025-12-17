c126cd832111beaa3e67082b92531678f06af3ca
RAHAT <83128928+Rahat4089@users.noreply.github.com>
2025-12-17 23:55:57 +0600

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,13 +4,8 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Madflix_Bots")
+    return web.json_response("GENZ")
 
 
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
